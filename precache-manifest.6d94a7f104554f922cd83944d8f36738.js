7328db0e1226dabf95f88dafba6afec4110fd105
Kira Bond <kors9442@gmail.com>
2020-03-29 23:30:27 +0300

    Updates

---
+++ precache-manifest.6d94a7f104554f922cd83944d8f36738.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28b538af601eb95e1c22b1fe2250866c",
+    "url": "/react-todo-app.github.io/index.html"
+  },
+  {
+    "revision": "b9af4b0c5805bcd846fa",
+    "url": "/react-todo-app.github.io/static/css/main.40bd7f75.chunk.css"
+  },
+  {
+    "revision": "17677f9491e0030e68d3",
+    "url": "/react-todo-app.github.io/static/js/2.99ff98d3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-todo-app.github.io/static/js/2.99ff98d3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b9af4b0c5805bcd846fa",
+    "url": "/react-todo-app.github.io/static/js/main.25361960.chunk.js"
+  },
+  {
+    "revision": "c60f355d8ea8da1f6c66",
+    "url": "/react-todo-app.github.io/static/js/runtime-main.c5511726.js"
+  }
+]);
